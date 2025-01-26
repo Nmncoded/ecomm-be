635535d3e7d804b0d1ed382ee08f50b9301e608c
Nmn <Naman.tortalabz@hotmail.com>
2025-01-26 21:24:37 +0530

    refreshtoken implemented

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -60,18 +60,57 @@ export const login = async (req, res) => {
     const token = jwt.sign(
       { id: user.id, email: user.email, role_id: user.role_id, contact_no: user.contact_no },
       process.env.JWT_SECRET,
-      { expiresIn: '1h' }
+      { expiresIn: '10s' }
+    );
+
+
+    const refresh_token = jwt.sign(
+      { id: user.id, email: user.email, role_id: user.role_id, contact_no: user.contact_no },
+      process.env.JWT_REFRESH_SECRET
     );
 
     delete user.password;
 
-    res.json({ success: true, message: 'Login successful',token, userDTO: user });
+    res.json({ success: true, message: 'Login successful',token, userDTO: user, refresh_token });
   } catch (error) {
     res.status(500).json({ error: 'Login failed' });
+  }
+};
+
+export const refreshToken = async (req, res) => {
+  try {
+    // console.log('refreshToken inside backend');
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    
+  
+    if (!token) return res.status(401).json({ error: 'Unauthorized' });
+
+    const decoded = jwt.verify(token, process.env.JWT_REFRESH_SECRET);
+    // console.log('refreshToken inside backend ------ ',decoded);
+
+    const user = await prisma.user.findFirst({ 
+      where: { email: decoded.email, contact_no: decoded.contact_no },
+      select: { id: true, email: true, role_id: true,contact_no: true,name: true }
+    });
+
+    if (!user) return res.status(403).json({ error: 'User not found' });
+
+    const access_token = jwt.sign(
+      { id: user.id, email: user.email, role_id: user.role_id, contact_no: user.contact_no },
+      process.env.JWT_SECRET,
+      { expiresIn: '1h' }
+    );
+
+
+    res.json({ success: true,access_token });
+  } catch (error) {
+    res.status(500).json({ error: 'refreshToken failed' });
   }
 };
 
 export const authController = {
   login,
   register,
+  refreshToken
 }
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { login, register } from '../controllers/auth.controller.js';
+import { login, register, refreshToken } from '../controllers/auth.controller.js';
 import { loginValidation, registerValidation } from '../validations/auth.validation.js';
+import { authenticateToken } from '../middleware/auth.middleware.js';
 const authRouter = express.Router();
 
 authRouter.post('/register', registerValidation ,register );
 
 authRouter.post('/login', loginValidation ,login );
+authRouter.get('/refreshToken' , refreshToken );
 
 export default authRouter
